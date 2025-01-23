da6d63b6e0dda0393a6bff8bccf28f7ed5953f6c
wormythegreat <lukealford06@gmail.com>
2025-01-23 19:18:57 +0000

    U_crit finished

--- Q4Ucrit.py
+++ Q4Ucrit.py
@@ -1,7 +1,29 @@
 from Q4Func import *
+from math import sqrt
 
 #Use from part 2
+#These are not right
 eq1 = -123
-eq2 = 123
+eq2 = -12
+eq3 = -1
+eq4 = 0
+eq5 = 1
+eq6 = 12
+eq7 = 123
 
+#####     E = 1/2mv^2 + V(s)   #####
 
+#At V(eq2), v = 0 so can work out the energy of the system with U = U_crit
+
+E_crit = V(eq2)
+
+#So at eq1 (which is where the roller coaster is launched form) the energy
+#equation looks like:
+
+#E_crit = 1/2mU^2 + V(eq1)
+
+#Rearranging for U
+
+U_crit = sqrt((2/m)*(E_crit-V(eq1)))
+
+print(U_crit)
